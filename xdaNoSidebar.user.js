38d707474f819fa63e30c3cc28f5e61953ca4395
Alistair1231 <alistair1231@gmail.com>
2018-08-18 15:03:10 +0200

    Update xdaNoSidebar.user.js

--- xdaNoSidebar.user.js
+++ xdaNoSidebar.user.js
@@ -6,7 +6,7 @@
 // @author       Alistair1231
 // @match        https://forum.xda-developers.com/*
 // @grant        none
-// @license GPL-3.0
+// @license GPL
 // @copyright 2018, Alistair1231 (https://openuserjs.org/users/Alistair1231)
 // @run-at document-idle
 // ==/UserScript==
