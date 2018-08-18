8e5c08c68fdde51f8353ca50fc3e15b9a69ab1a4
Alistair1231 <alistair1231@gmail.com>
2018-08-18 14:56:11 +0200

    Create xdaNoSidebar.user.js

---
+++ xdaNoSidebar.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         XDA Sidebar hider
+// @namespace    https://github.com/Alistair1231/
+// @version      0.0.1
+// @description  Hides the sidebar with jquery
+// @author       Alistair1231
+// @match        https://forum.xda-developers.com/*
+// @grant        unsafeWindow
+// @license GPL-3.0
+// @copyright 2018, Alistair1231 (https://openuserjs.org/users/Alistair1231)
+// @run-at document-idle
+// ==/UserScript==
+
+// ==OpenUserJS==
+// @author Alistair1231
+// ==/OpenUserJS==
+
+setTimeout(function(){
+	var scr = document.createElement('script');
+	scr.type="text/javascript";
+	scr.src="https://gistcdn.githack.com/Alistair1231/61d55e03deaad64e5096bda0639167f8/raw/a10225938c3069ace6ba36d9d7d514637ccc9ba2/xdaNoSidebar.js";
+	document.getElementsByTagName('head')[0].appendChild(scr);
+	//document.body.appendChild(scr);
+}, 200);
